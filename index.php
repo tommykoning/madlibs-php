f649f8aa9ad7aa2f56bc9e53c83d3f5ad4e33b12
tommy koning <99039195@mydavinci.nl>
2017-03-22 08:41:35 +0100

    start of project

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>madLips</title>
+	</head>
+	<body>
+		<h1>madLips</h1>
+		<article>
+		<header>
+			<a href="">er is paniek</a>
+			<a href="">onkunde</a>
+		</header>
+		<form>
+			<label for="huisdier">welk huis dier zou je graag willen hebben ?</label>
+			<input type="text" name="huisdier" id="huisdier"><br>
+			<label for="persoon">wie is de belangrijkste persoon in je leven ?</label>
+			<input type="text" name="persoon" id="persoon"><br>
+			<label for="land">in welk land zou je graag willen wonen ?</label>
+			<input type="text" name="land" id="land"><br>
+			<label for="verveel">wat doe je als jij je verveeld ?</label>
+			<input type="text" name="verveel" id="verveel"><br>
+			<label for="speelgoed">met</label>
+		</form>
+		</article>
+	</body>
+</html>
